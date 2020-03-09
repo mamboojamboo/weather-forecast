5b594442e4aaa0254db14eef63f9ecaecdff1d47
Mikhail <mvafonin@yandex.ru>
2020-03-09 22:31:39 +0300

    feat(SearchBar-component): add component, kea logic

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import { useValues, useActions } from 'kea';
+import appLogic from '../../logic/appLogic';
 
-const TopBar = () => (
-    <div>
-      SearchBar
-    </div>
-);
+
+import {
+  BarForm,
+  InputInBarForm,
+  ButtonInBarForm
+} from './units';
+
+const TopBar = () => {
+  const { weather, isLoading } = useValues(appLogic);
+  const { updateWeatherAsync } = useActions(appLogic);
+
+  console.log(isLoading);
+  console.log(weather);
+
+
+  return (
+    <BarForm onSubmit={(event) => updateWeatherAsync(event)}>
+      <InputInBarForm
+        type='text'
+        name='inputCity'
+        autoComplete='off'
+        placeholder={!weather.error ? 'city' : 'enter city'}
+        // defaultValue={weather.city}
+      />
+      <InputInBarForm
+        type='text'
+        name='inputCountry'
+        autoComplete='off'
+        placeholder={!weather.error ? 'country' : 'enter country'}
+        // defaultValue={weather.country}
+      />
+      <ButtonInBarForm>Get weather</ButtonInBarForm>
+    </BarForm>
+  );
+};
 
 export default TopBar;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import styled from '@emotion/styled';
+import { getContext, resetContext } from 'kea';
+import { Provider } from 'react-redux';
+import thunkPlugin from 'kea-thunk';
 
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import SearchBar from './components/SearchBar';
 import WeatherCard from './components/WeatherCard';
 
+resetContext({
+  createStore: true,
+  plugins: [thunkPlugin]
+});
+
+const AppWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  box-sizing: border-box;
+  align-items: center;
+  background: #142136;
+`;
 
 function App() {
   return (
-    <Fragment>
-      <SearchBar/>
-      <WeatherCard/>
-    </Fragment>
+    <Provider store={getContext().store}>
+      <AppWrapper>
+        <SearchBar/>
+        <WeatherCard/>
+      </AppWrapper>
+    </Provider>
   );
 }
 
