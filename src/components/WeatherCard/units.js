513df3cd347d5b6b6e71645ddb65597621d6cba6
Mikhail <mvafonin@yandex.ru>
2020-03-18 17:15:13 +0300

    feat(MainPage): add page

---
+++ src/components/WeatherCard/units.js
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+const CardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const CardTitle = styled.span`
+  font-size: 2rem;
+  color:white;
+  text-transform: uppercase
+`;
+
+
+export { CardWrapper, CardTitle };
