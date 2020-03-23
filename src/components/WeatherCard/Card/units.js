8741aeb06386a067cd5bd62ed519d74d6e59aec0
Mikhail <mvafonin@yandex.ru>
2020-03-23 20:42:15 +0300

    feat(WeatherIcoms): npm pack, logic

--- src/components/WeatherCard/Card/units.js
+++ src/components/WeatherCard/Card/units.js
@@ -2,38 +2,34 @@ import styled from '@emotion/styled';
 
 const Wrapper = styled.div((props) => ({
   display: !props.isLoading ? 'flex' : 'none',
-  flexDirection: 'column'
+  flexDirection: 'column',
+  alignItems: 'center',
+  textTransform: 'uppercase',
+  fontSize: '2rem',
+  color: 'white'
 }));
 
 const City = styled.span((props) => ({
   display: props.city ? 'inline' : 'none',
-  fontSize: '2rem',
-  color: 'white',
-  textTransform: 'uppercase'
+  fontSize: '4rem'
 }));
 
 const WeatherIcon = styled.div((props) => ({
-  display: props.icon ? 'flex' : 'none'
+  display: props.icon ? 'inline' : 'none',
+  fontSize: '8rem'
 }));
 
+
 const TempCelsius = styled.span((props) => ({
-  display: props.tempCelsius ? 'inline' : 'none',
-  fontSize: '2rem',
-  color: 'white',
-  textTransform: 'uppercase'
+  display: props.tempCelsius ? 'inline' : 'none'
 }));
 
 const TempMinMax = styled.span((props) => ({
-  display: props.tempMin && props.tempMax ? 'inline' : 'none',
-  fontSize: '2rem',
-  color: 'white',
-  textTransform: 'uppercase'
+  display: props.tempCelsiusMin && props.tempCelsiusMax ? 'inline' : 'none'
 }));
 
 const Description = styled.span((props) => ({
-  display: props.description ? 'inline' : 'none',
-  fontSize: '2rem',
-  color: 'white'
+  display: props.description ? 'inline' : 'none'
 }));
 
 export {
