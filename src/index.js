a49b2bd009fd78c7378ff3f1d0711424909daf13
Mikhail <mvafonin@yandex.ru>
2020-03-26 20:12:23 +0300

    feat(globalStyles): add globalStyles, remove index.css, index.sass

--- src/index.js
+++ src/index.js
@@ -2,20 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 import store from './store/initialStore';
 import MainPage from './pages/MainPage';
 
+import GlobalStyles from './globalStyles';
 
-function App() {
-  return (
+const App = () => (
     <Provider store={store}>
+      <GlobalStyles/>
       <MainPage/>
     </Provider>
-  );
-}
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
