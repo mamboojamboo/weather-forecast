cd03abd345a6a08eb94c4d3a11dfe8542cb70835
Mikhail Afonin <afoninmv@gmail.com>
2021-10-26 16:38:49 +0300

    refactor: move globalStyles to app folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { MainPage } from './pages/MainPage';
-import GlobalStyles from './globalStyles';
+import GlobalStyles from './app/globalStyles';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
